d2666378d3d1568893658889626a0a1761320d23
luuhuy2402 <xeoxeo1895@gmail.com>
2025-04-06 13:02:10 +0700

    Show product and format number

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -13,3 +13,19 @@ export function isAxiosUnprocessableEntityError<FormError>(
         error.response?.status === HttpStatusCode.UnprocessableEntity
     );
 }
+
+//Format lại giá
+export function formatCurrency(currency: number) {
+    return new Intl.NumberFormat("de-DE").format(currency);
+}
+
+//Format lượt hàng đã bán
+export function formatNumberToSocialStyle(value: number) {
+    return new Intl.NumberFormat("en", {
+        notation: "compact",
+        maximumFractionDigits: 1,
+    })
+        .format(value)
+        .replace(".", ",")
+        .toLowerCase();
+}
